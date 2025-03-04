6253584435ab34af5c362e8c9348515713bd0467
rahmates <hishamrahma683@gmail.com>
2025-03-04 17:15:44 +0200

    save the new changes

--- Academy.Repo/UnitOfWork.cs
+++ Academy.Repo/UnitOfWork.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Academy.Core;
+using Academy.Core.Repositories;
+using Academy.Repo.Data;
+using Academy.Repo.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,31 @@ using System.Threading.Tasks;
 
 namespace Academy.Repo
 {
-    internal class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly AcademyContext _context;
+        private Hashtable _repositories;
+        public UnitOfWork( AcademyContext context) 
+        {
+            _context = context;
+            _repositories = new Hashtable();
+        }
+        public async Task<int> CompleteAsync()
+        => await _context.SaveChangesAsync();
+        
+
+        public IGenaricRepository<T> Repository<T>() where T : class
+        {
+            var type = typeof(T).Name;
+
+            // لو الريبو مش موجود ضيفه 
+            if(!_repositories.ContainsKey(type))
+            {
+                var repository = new GenaricRepository<T>(_context);
+                _repositories.Add(type, repository);
+            }
+            // بعد ما ضيفته رجعهولي بقي اشتغل عليه 
+            return _repositories[type] as IGenaricRepository<T>;
+        }
     }
 }
